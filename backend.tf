c95f0e441fffdde0bafac7e3b81f8451e672b8e7
yusuke sugiyama <yus-sugiyama@uninosushi.com>
2022-07-18 16:06:37 +0900

    fix ci-cd yml

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "unisushi-oregon-tfstate"
-    key    = "all-state/terraform.tfstate"
-    region = "us-west-2"
-    // profile        = "unisushi_terraform"
+    bucket         = "unisushi-oregon-tfstate"
+    key            = "all-state/terraform.tfstate"
+    region         = "us-west-2"
+    profile        = "unisushi_terraform"
     dynamodb_table = "TerraformStateLockTable"
   }
 }
